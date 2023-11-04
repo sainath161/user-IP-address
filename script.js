034714bdb9df1b0e0a1f264674eb3f5947cddab2
sainath161 <sainathsmart1234@gmail.com>
2023-11-04 11:30:11 +0530

    User IP address

--- script.js
+++ script.js
@@ -1,70 +1,85 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const getInfoButton = document.getElementById("getInfoButton");
+    const ipAddressElement = document.getElementById("ip-address");
+    const locationElement = document.getElementById("location");
+    const timezoneElement = document.getElementById("timezone");
+    const searchBox = document.getElementById("search-box");
+    const postOfficeList = document.getElementById("post-office-list");
+    const googleMap = document.getElementById("google-map");
+    const getStartedButton = document.getElementById("get-started-button");
 
-    getInfoButton.addEventListener("click", async () => {
-        try {
-            const userIP = await getUserIPAddress();
-            const ipInfo = await getIPInformation(userIP);
+    let userIP = null;
+    let postOfficesData = [];
 
-            // Open a new page with the IP information
-            openNewPage(userIP, ipInfo);
-        } catch (error) {
-            console.error("Error:", error);
+    getStartedButton.addEventListener("click", function () {
+        if (!userIP) {
+            fetchUserIPAddress();
         }
+        fetchUserInformation(userIP);
     });
 
-    async function getUserIPAddress() {
-        try {
-            const response = await fetch('https://api.ipify.org?format=json');
-            const data = await response.json();
-            return data.ip;
-        } catch (error) {
-            console.error("Error getting user's IP address: ", error);
-            throw error;
-        }
+    searchBox.addEventListener("input", function () {
+        const searchText = searchBox.value.trim().toLowerCase();
+        const filteredOffices = postOfficesData.filter(office =>
+            office.Name.toLowerCase().includes(searchText) || office.BranchType.toLowerCase().includes(searchText)
+        );
+        displayPostOffices(filteredOffices, postOfficeList);
+    });
+
+    function fetchUserIPAddress() {
+        fetch('https://api.ipify.org?format=json')
+            .then(response => response.json())
+            .then(data => {
+                userIP = data.ip;
+                ipAddressElement.textContent = userIP;
+            })
+            .catch(error => {
+                console.error("Error getting user's IP address: ", error);
+            });
     }
 
-    async function getIPInformation(userIP) {
-        try {
-            const response = await fetch(`https://ipapi.co/${userIP}/json/`);
-            if (!response.ok) {
-                throw new Error('Error fetching IP information');
-            }
-            const data = await response.json();
-            return {
-                location: `${data.city}, ${data.region}, ${data.country_name}`,
-                timezone: data.timezone,
-                pincode: data.postal,
-            };
-        } catch (error) {
-            console.error("Error getting IP information: ", error);
-            throw error;
-        }
+    function fetchUserInformation(userIP) {
+        fetch(`https://ipapi.co/${userIP}/json/`)
+            .then(response => response.json())
+            .then(data => {
+                locationElement.textContent = data.city ? data.city + ', ' + data.region + ', ' + data.country_name : "Location not available";
+                timezoneElement.textContent = data.timezone ? data.timezone : "Timezone not available";
+                
+                // Set the pincode automatically based on the user's location data
+                const pincode = data.postal;
+                
+                // Display the user's location on Google Maps
+                displayLocationOnMap(data.latitude, data.longitude);
+
+                fetchPostOffices(pincode);
+            })
+            .catch(error => {
+                console.error("Error fetching IP information: ", error);
+            });
     }
 
-    function openNewPage(userIP, ipInfo) {
-        const newPage = window.open("", "_blank");
+    function displayLocationOnMap(lat, long) {
+        const googleMapsUrl = `https://www.google.com/maps/embed/v1/place?q=${lat},${long}&key=AIzaSyA8nH8iWGdG31PusBw0XJTQt4h5v93B-zg`; // Replace 'YOUR_API_KEY' with your Google Maps API key
+        googleMap.src = googleMapsUrl;
+    }
 
-        newPage.document.write(`
-            <html>
-            <head>
-                <link rel="stylesheet" type="text/css" href="styles.css">
-            </head>
-            <body>
-                <div class="container">
-                    <h1>IP Information and Post Offices</h1>
-                    <div id="ipInfo">
-                        <p><strong>IP Address:</strong> ${userIP}</p>
-                        <p><strong>Location:</strong> ${ipInfo.location}</p>
-                        <p><strong>Timezone:</strong> ${ipInfo.timezone}</p>
-                    </div>
-                    <div id="map"></div>
-                    <input type="text" id="searchBox" placeholder="Search Post Offices">
-                    <ul id="postOffices"></ul>
-                </div>
-                <script src="newPageScript.js"></script>
-            </body>
-            </html>
-        `);
+    function fetchPostOffices(pincode) {
+        fetch(`https://api.postalpincode.in/pincode/${pincode}`)
+            .then(response => response.json())
+            .then(data => {
+                postOfficesData = data[0].PostOffice;
+                displayPostOffices(postOfficesData, postOfficeList);
+            })
+            .catch(error => {
+                console.error("Error fetching post offices: ", error);
+            });
+    }
+
+    function displayPostOffices(postOffices, postOfficesList) {
+        postOfficesList.innerHTML = "";
+        postOffices.forEach(office => {
+            const listItem = document.createElement("li");
+            listItem.textContent = `${office.Name} - ${office.BranchType}`;
+            postOfficesList.appendChild(listItem);
+        });
     }
 });
